9fbaaca79113338abe2bf820a8e71998ee874ece
aashishkhadka1992 <42074487+aashishkhadka1992@users.noreply.github.com>
2019-04-01 15:17:54 -0400

    Add files via upload

---
+++ NewsAPI_Credentials.py
@@ -0,0 +1,2 @@
+### News API credentials ###
+api_key = '2863a4b8c7304f77b614fda3779f6203'
---
+++ Twitter_Credentials.py
@@ -0,0 +1,6 @@
+### Twitter Credentials ###
+
+consumer_key = '0APfsWAvvzzGYdte0jeZ3aGkm'
+consumer_secret_key = 'qSOWgmmZPD9k3Vtq8gqOUvivR9Ru4yxvfwb2tfDZv74knhUcXV'
+access_key = '2776112929-awXLdMidkvepR0aEZqiO0aVCymUfFAycRikWCO2'
+access_secret_key = 'aydOP4m35OSGwvGhxt2YKLNAEYoXNaxqxTFEMT9oGdOhy'
